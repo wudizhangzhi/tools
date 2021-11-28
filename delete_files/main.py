45e8574103419a3b070ea383d5ae3656a20b7c84
wudizhangzhi <wudizhangzhi@163.com>
2021-11-28 20:02:08 +0800

    update

--- delete_files/main.py
+++ delete_files/main.py
@@ -15,9 +15,9 @@ Options:
 from docopt import docopt
 import os
 import re
+from collections import namedtuple
 
-
-
+FD = namedtuple('FD', ['filepath', 'filename', 'size', 'suffix'])
 
 def run(**kwargs):
     """[删除无用的文件]
@@ -49,21 +49,33 @@ def run(**kwargs):
       suffix = file.split('.')[-1].lower()
       size = os.path.getsize(filepath) # 字节 Byte
       sizeMB = round(size / 1000 / 1000, 2)
+      fd = FD(
+        filepath=filepath,
+        filename=file,
+        size=sizeMB,
+        suffix=suffix
+      )
       # print(f'文件名: {file} 文件类型: {suffix} 大小: {sizeMB}MB')
       if suffix not in _type:
-        to_be_deleted.append((filepath, file, "后缀不对"))
+        to_be_deleted.append((fd, "后缀不对"))
         continue
       if sizeMB < _size:
-        to_be_deleted.append((filepath, file, f"文件太小: {sizeMB}MB"))
+        to_be_deleted.append((fd, f"文件太小: {sizeMB}MB"))
         continue
       if _pattern and not re.match(_pattern, file):
-        to_be_deleted.append((filepath, file, f"名称不匹配"))
+        to_be_deleted.append((fd, f"名称不匹配"))
         continue
 
     if len(to_be_deleted) > 0:
-      for fullpath, filename, reason in to_be_deleted:
-        print(f"原因: {reason} 删除: {filename}")
-      print(f"共: {len(to_be_deleted)}个")
+      total_size = 0
+      for fd, reason in to_be_deleted:
+        total_size+=fd.size
+        print(f"原因: {reason} 删除: {fd.filename}")
+      if total_size > 1000:
+        total_size_display = f"{round(total_size/1000, 2)}GB"
+      else:
+        total_size_display = f"{round(total_size, 2)}MB"
+      print(f"共: {len(to_be_deleted)}个, {total_size_display}")
       confirm = input("请确认是否删除(y/n):")
       if confirm.lower() in ['y', 'yes']:
         for fullpath, _, _ in to_be_deleted:
